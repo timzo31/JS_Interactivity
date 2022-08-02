74546ad3d32daa89becb0a10285f0e665cac2eaa
timzo31 <madty31@gmail.com>
2022-08-02 14:24:08 +0800

    DOM Output and Variables.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("Hello world");
+////////////////// WEEK 1 ///////////////////
+/////// JAVASCRIPT: Generate OUTPUT /////////
+
+//alert("Hello world");
+//prompt("Put your name:");
+document.write("<h1>Time to learn Javascript</h1>");
+
+document.getElementById("test").innerHTML = "Hello World!";
+
+console.log("Leave a secret");
+console.log(window.location);
+
+///////// VARIABLES /////////////
+
+const name = prompt("Enter your name: ");
+document.write(name);
+document.write("<h1>" + name + "</h1>");
+document.write(document.baseURI + "<br>");
+document.write(document.title + "<br>");
